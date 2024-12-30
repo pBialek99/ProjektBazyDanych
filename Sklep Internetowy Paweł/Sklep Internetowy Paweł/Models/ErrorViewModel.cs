9a38a9a0104283c80d253b84d502a16eb2489692
pBialek99 <pawel99bialek@gmail.com>
2024-12-30 13:22:27 +0100

    Add files via upload

---
+++ Sklep Internetowy Paweł/Sklep Internetowy Paweł/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sklep_Internetowy_Paweł.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
